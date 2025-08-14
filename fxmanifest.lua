68f47929530b537069e59411875c109a633f6046
HeisenbergJr49 <carstensiltman@gmail.com>
2025-08-14 10:38:33 +0200

    Add fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'HeisenbergJr49'
+description 'ESX Radio Members UI'
+version '1.0.0'
+
+client_scripts {
+    'client/**/*.lua'
+}
+
+server_scripts {
+    'server/**/*.lua'
+}
+
+files {
+    'html/index.html',
+    'html/styles.css',
+    'html/script.js',
+    'locales/de.lua',
+    'locales/en.lua'
+}
+
+ui_page 'html/index.html'
